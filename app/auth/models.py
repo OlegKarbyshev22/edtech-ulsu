1fc3599c70ccd87daafdc9f0789679ea24706ea1
Oleg Karbyshev <110419971+OlegKarbyshev22@users.noreply.github.com>
2026-07-01 23:08:53 +0400

    refactor: rename auth model class

--- app/auth/models.py
+++ app/auth/models.py
@@ -2,14 +2,14 @@ from sqlalchemy import String, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
 
-class RoleModel(Base):
+class Role(Base):
     __tablename__ = "roles"
     
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    users: Mapped[list["UserModels"]] = relationship("UserModels", back_populates="role")
+    users: Mapped[list["User"]] = relationship("User", back_populates="roles")
 
-class UserModels(Base):
+class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
@@ -17,4 +17,4 @@ class UserModels(Base):
     login: Mapped[str] = mapped_column(String(100), unique=True, index=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     role_id: Mapped[int] = mapped_column(Integer, ForeignKey("roles.id"), nullable=False)
-    role: Mapped['RoleModel'] = relationship("RoleModel", back_populates="users")
+    roles: Mapped['Role'] = relationship("Role", back_populates="users")
